f88821a8df857e3d962be99a6dba9dfac120735d
natdm02 <natalidimarino2002@gmail.com>
2024-10-09 15:31:01 +0200

    [add] scafolding

--- index.php
+++ index.php
@@ -4,9 +4,24 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>php-opp-1</title>
+    <!--bootstrap-->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <!--css-->
+    <link rel="stylesheet" href="./assets/css/style.css">
+    <!--vuejs-->
+    <script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
+    
 </head>
 
 <body>
-    
+    <main>
+        <div class="container">
+
+        <div class="row">
+            oop-1
+        </div>
+
+        </div>
+    </main>
 </body>
 </html>
